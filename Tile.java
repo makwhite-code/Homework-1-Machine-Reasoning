8e18364016b5584db0a6448e19b4dfa2d4336a0f
sagalvan <sagalvan@davidson.edu>
2026-09-14 13:08:01 -0400

    Implementing tile class to Board
    Moved the isAdjacent and moveTile functions to the board class. Also implemented the shuffle function and the isSolved function.

--- Tile.java
+++ Tile.java
@@ -33,80 +33,80 @@ public class Tile {
 
 
     // Move/switch this tile with the empty space
-    public boolean isAdjacent(int value) {
-    // TODO:
-    // 1. Find the tile with this value
-    // 2. Find the empty tile
-    // 3. Check if they are adjacent
-    // 4. If they are adjacent, swap them
-    // 5. Update row/col for both tiles
-    // 6. Return true
-    // 7. Otherwise return false
+    // public boolean isAdjacent(int value) {
+    // // TODO:
+    // // 1. Find the tile with this value
+    // // 2. Find the empty tile
+    // // 3. Check if they are adjacent
+    // // 4. If they are adjacent, swap them
+    // // 5. Update row/col for both tiles
+    // // 6. Return true
+    // // 7. Otherwise return false
 
-    int valrow = -1;
-    int valcol = -1;
-    int emptyrow = -1;
-    int emptycol = -1;
+    // int valrow = -1;
+    // int valcol = -1;
+    // int emptyrow = -1;
+    // int emptycol = -1;
 
-    for (int i = 0; i < 3; i++){
-        for (int j = 0; j < 3; j++){
-            if (board[i][j].getData()== value){
-                valrow = i;
-                valcol = j;
-            }
-            if (board[i][j].getData() == 0){
-                emptyrow = i;
-                emptycol = j;
-            }
-        }
-    }
-    if (valrow == emptyrow){
-        if(valcol == emptycol + 1 || valcol == emptycol - 1){
-            return true;
-        }
+    // for (int i = 0; i < 3; i++){
+    //     for (int j = 0; j < 3; j++){
+    //         if (board[i][j].getData()== value){
+    //             valrow = i;
+    //             valcol = j;
+    //         }
+    //         if (board[i][j].getData() == 0){
+    //             emptyrow = i;
+    //             emptycol = j;
+    //         }
+    //     }
+    // }
+    // if (valrow == emptyrow){
+    //     if(valcol == emptycol + 1 || valcol == emptycol - 1){
+    //         return true;
+    //     }
         
-    }
-    if (valcol == emptycol){
-        if(valrow == emptyrow + 1 || valrow == emptyrow - 1){
-            return true;
-        }
+    // }
+    // if (valcol == emptycol){
+    //     if(valrow == emptyrow + 1 || valrow == emptyrow - 1){
+    //         return true;
+    //     }
     
-    }
+    // }
 
-    return false;
+    // return false;
 
 
-    }
+    // }
 
-    public boolean moveTile(int value) {
-    if (isAdjacent(value)) {
-        // swap the tiles
-        int valrow = -1;
-        int valcol = -1;
-        int emptyrow = -1;
-        int emptycol = -1;
+    // public boolean moveTile(int value) {
+    // if (isAdjacent(value)) {
+    //     // swap the tiles
+    //     int valrow = -1;
+    //     int valcol = -1;
+    //     int emptyrow = -1;
+    //     int emptycol = -1;
 
-        for (int i = 0; i < 3; i++){
-            for (int j = 0; j < 3; j++){
-                if (board[i][j].getData()== value){
-                    valrow = i;
-                    valcol = j;
-                }
-                if (board[i][j].getData() == 0){
-                    emptyrow = i;
-                    emptycol = j;
-                }
-            }
-        }
+    //     for (int i = 0; i < 3; i++){
+    //         for (int j = 0; j < 3; j++){
+    //             if (board[i][j].getData()== value){
+    //                 valrow = i;
+    //                 valcol = j;
+    //             }
+    //             if (board[i][j].getData() == 0){
+    //                 emptyrow = i;
+    //                 emptycol = j;
+    //             }
+    //         }
+    //     }
 
-        Tile temp = board[emptyrow][emptycol].getData();
-        board[emptyrow][emptycol].getData() = board[valrow][valcol].getData();
-        board[valrow][valcol].getData() = temp;
-        return true;
+    //     Tile temp = board[emptyrow][emptycol].getData();
+    //     board[emptyrow][emptycol].getData() = board[valrow][valcol].getData();
+    //     board[valrow][valcol].getData() = temp;
+    //     return true;
 
-        }
-    return false;
-    }   
+    //     }
+    // return false;
+    // }   
 
 
 
@@ -115,6 +115,8 @@ public class Tile {
         this.row = row;
         this.col = col;
     }
+
+    // Optional: update the tiles data
 
 
     // Optional: useful for printing/debugging
