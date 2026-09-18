567c3da3dc9995320427dcc030d1a827b860af84
makwhite-code <makwhite@davidson.edu>
2026-09-18 10:44:06 -0400

    Test case for astar

--- Main.java
+++ Main.java
@@ -1,8 +1,11 @@
+
+
 public class Main {
     public static void main(String[] args) {
 
-        int[] nums = {1, 2, 0, 3, 4, 5, 6, 7, 8};
-
+        int[] nums = {0, 1, 2,
+            3, 4, 5,
+            6, 7, 8};
         Board start = new Board(nums);
 
         AStar.solve(start);
